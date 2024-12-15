a82aecf54699cb7a4768ccac05c572bd33c5318b
MuchiriKinyua <muchirikinyua2000@gmail.com>
2024-12-15 14:54:51 +0300

    Aligning the dates

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-const date = document.getElementById("date")
-const day = document.getElementById("day")
-const month = document.getElementById("month")
-const year = document.getElementById("year")
+
