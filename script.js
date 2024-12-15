f65a872cf66bf4151402055074a09f8cd4dc0b98
MuchiriKinyua <muchirikinyua2000@gmail.com>
2024-12-15 14:43:30 +0300

    Skeleton

---
+++ script.js
@@ -0,0 +1,4 @@
+const date = document.getElementById("date")
+const day = document.getElementById("day")
+const month = document.getElementById("month")
+const year = document.getElementById("year")
